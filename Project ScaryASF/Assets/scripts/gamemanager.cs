4701f4a6ea9f488086d01d0d18804c10ebe46509
Dazzamon12 <dazimon99@hotmail.com>
2024-09-04 20:07:49 +0930

    Text mesh pro importing

--- Project ScaryASF/Assets/scripts/gamemanager.cs
+++ Project ScaryASF/Assets/scripts/gamemanager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class gamemanager : MonoBehaviour
 {
+
+    public TextMeshPro bar;
+    public TextMeshPro health;
+
     // Start is called before the first frame update
     void Start()
     {
