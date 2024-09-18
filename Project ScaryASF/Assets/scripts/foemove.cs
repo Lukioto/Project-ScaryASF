c44d78983e49f40ccf097706e7418dfb41713f31
Dazzamon12 <dazimon99@hotmail.com>
2024-09-18 20:15:06 +0930

    nono

--- Project ScaryASF/Assets/scripts/foemove.cs
+++ Project ScaryASF/Assets/scripts/foemove.cs
@@ -9,20 +9,15 @@ public class foemove : MonoBehaviour
 
     public Transform _foe;
 
-    private GameObject _player;
+
 
     void Awake()
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
-
         
     }
 
     void Update()
     {
-        Vector3 targetPostition = new Vector3(_player.transform.position.x,
-                                       _player.transform.position.y,
-                                       _player.transform.position.z);
-        this.transform.LookAt(targetPostition);
+        transform.rotation = Quaternion.Euler(90f, Camera.main.transform.rotation.eulerAngles.y, -90f);
     }
 }
