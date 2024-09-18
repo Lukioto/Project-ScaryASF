4697a54d28fc98938ccb5eec653180957dbc5382
Lukioto <77881026+Lukioto@users.noreply.github.com>
2024-09-18 19:47:24 +0930

    looking

---
+++ Project ScaryASF/Assets/Scripts/SockManJumpscare.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SockManJumpscare : MonoBehaviour
+{
+    public Image image;
+    public AudioSource audioSource;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(SM_Jumpscare());
+    }
+
+    IEnumerator SM_Jumpscare()
+    {
+        image.enabled = true;
+        audioSource.Play();
+        yield return new WaitForSeconds(1f);
+        image.enabled = false;
+    }
+}
