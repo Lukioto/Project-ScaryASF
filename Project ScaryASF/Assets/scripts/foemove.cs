f926e57ce524b8dada8f75f8678544d5b0a53cc0
Dazzamon12 <dazimon99@hotmail.com>
2024-09-18 17:45:17 +0930

    small changes

--- Project ScaryASF/Assets/scripts/foemove.cs
+++ Project ScaryASF/Assets/scripts/foemove.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class foemove : MonoBehaviour
 {
+
+    public Transform _foe;
+
+    private GameObject _player;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _foe.LookAt(_player.transform);
     }
 }
